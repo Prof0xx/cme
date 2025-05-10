5c304f1682018353e743d1de4cbc4d84d8e0e86f
Prof0xx <gemzfarnsworth@gmail.com>
2025-05-10 05:27:37 +0200

    Fix more type imports for verbatimModuleSyntax

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
-import express, { type Request, Response, NextFunction } from "express";
+import express from "express";
+import type { Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import session from "express-session";
